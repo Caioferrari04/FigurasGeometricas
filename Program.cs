26d363e09589bba8e043a84d3c28b6834490e532
Caioferrari04 <caioferrari0484@gmail.com>
2022-04-22 10:20:36 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Quadrado q1 = new Quadrado(10.0);
+
+Console.WriteLine(q1.Lado);
+
+q1.Lado = 5.0;
+
+Console.WriteLine(q1.Area);
+
+Console.WriteLine(q1.Perimetro); 
+
+Retangulo r1 = new Retangulo(10.0, 20.0);
+
+Console.WriteLine(r1.Altura);
+
+Console.WriteLine(r1.Base);
+
+Console.WriteLine(r1.Perimetro);
+
+Console.WriteLine(r1.Area);
